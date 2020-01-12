772312ce80b663a3808f046f53acc87cb3da09fc
primate-2016 <moo@moo.moo>
2020-01-12 15:11:53 +0000

    get a graph out

--- stockbot.py
+++ stockbot.py
@@ -1,31 +1,33 @@
 import os
 # import pandas as pd
-import datetime
+# import datetime
 import pandas_datareader.data as web
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import math
 import numpy as np
-from sklearn import preprocessing
+from datetime import datetime, timedelta
+from sklearn import preprocessing, model_selection, svm
+from sklearn.linear_model import LinearRegression
 # from pandas import DataFrame
 from matplotlib import style
 
-start = datetime.datetime(2010, 1, 1)
-end = datetime.datetime(2017, 1, 11)
+start = datetime(2017, 1, 1)
+end = datetime(2019, 1, 11)
 av_api_key = os.getenv('ALPHAVANTAGE_API_KEY')
 
 df = web.DataReader("LON:BARC", 'av-daily', start=start, end=end, api_key=av_api_key)
-print(df)
+# print(df)
 close_px = df['close']
 hundred_day_moving_avg = close_px.rolling(window=100).mean()
 ten_day_moving_avg = close_px.rolling(window=10).mean()
 
-# Adjusting the size of matplotlib
-mpl.rc('figure', figsize=(8, 7))
-mpl.__version__
+# # Adjusting the size of matplotlib
+# mpl.rc('figure', figsize=(8, 7))
+# mpl.__version__
 
-# Adjusting the style of matplotlib
-style.use('ggplot')
+# # Adjusting the style of matplotlib
+# style.use('ggplot')
 
 # close_px.plot(label='BARC')
 # hundred_day_moving_avg.plot(label='100d_moving_avg')
@@ -46,14 +48,14 @@ dfreg['HL_PCT'] = (df['high'] - df['low']) / df['close'] * 100.0
 # create a new column showing the % change on the day
 dfreg['PCT_CHANGE'] = (df['close'] - df['open']) / df['open'] * 100.0
 
-print(dfreg)
+# print(dfreg)
 
 # Drop missing value
 dfreg.fillna(value=-99999, inplace=True)
 
 # figure out how big 1% of the data is and use this difference to forecast
 forecast_out = int(math.ceil(0.01 * len(dfreg)))
-print(forecast_out)
+# print(forecast_out)
 # i.e. if the length of the table is 1000 this will return 10 (i.e. 0.01 or 1%)
 
 # we want to predict the AdjClose, so use the 'close' column as the data source
@@ -63,21 +65,52 @@ dfreg['label'] = dfreg[forecast_col].shift(-forecast_out)
 print(dfreg)
 
 # this just appears to be dropping the label column we just added?
-X = np.array(dfreg.drop(['label'], 1))
+X = np.array(dfreg.drop(['label', forecast_col], 1))
 
-print(X)
+# print(X)
 
 # Scale the X so that everyone can have the same distribution for linear regression
 # .scale() standardizes a dataset along an axis - looks like it smooths out the data
 # so models don't get flipped out by weird variances
 X = preprocessing.scale(X)
 
-print(X)
+# print(X)
 
 # Finally We want to find Data Series of late X and early X (train) for model generation and evaluation
-X_lately = X[-forecast_out:]
-X = X[:-forecast_out]
+X_end = X[-forecast_out:]
 
+# print(X_end)
+X_early = X[:-forecast_out]
+# print(X_early)
 # Separate label and identify it as y
+
+dfreg.dropna(inplace=True)
 y = np.array(dfreg['label'])
-y = y[:-forecast_out]
+# y_early = y[:-forecast_out]
+# print(y_early)
+
+X_train, X_test, y_train, y_test = model_selection.train_test_split(X_early, y, test_size=0.2)
+clf = LinearRegression(n_jobs=-1)
+clf.fit(X_train, y_train)
+
+confidence = clf.score(X_test, y_test)
+
+forecast_set = clf.predict(X_end)
+df['Forecast'] = np.nan
+
+last_date = df.iloc[-1].name
+last_unix = last_date
+next_unix = last_unix + str(timedelta(days=1))
+
+for i in forecast_set:
+    next_date = next_unix
+    next_unix += str(timedelta(days=1))
+    df.loc[next_date] = [np.nan for _ in range(len(df.columns)-1)]+[i]
+
+
+df['close'].plot()
+df['Forecast'].plot()
+plt.legend(loc=4)
+plt.xlabel('Date')
+plt.ylabel('Price')
+plt.show()
